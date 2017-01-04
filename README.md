3306af92280ae360bd31887a4473339d2c74f03e
Minh Ta <Minh Ta>
2017-01-04 22:12:40 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# File Metadata Microservice API
+
+File Metadata Microservice Api repository.
+
+You can run it at https://mks-file-metadata-api.herokuapp.com/
+
+## Requirements
+
+* Expressjs:
+
+  `npm install express --save`
+
+* dotenv:
+
+  `npm install dotenv --save`
+
+* multer:
+
+  `npm install multer --save`
+
+## User Stories
+
+* I can submit a FormData object that includes a file upload.
+
+* When I submit something, I will receive the file size in bytes within the JSON response.
---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+require('dotenv').config({ silent: true });
+var multer  = require('multer');
+var fileSizeLimit = 2000000;
+var upload = multer({
+    limits: { fileSize: fileSizeLimit, files: 1 }
+}).single('mks-upload');
+
+app.set('view options', {layout: false});
+app.use(express.static(__dirname + '/views'));
+
+var port = process.env.PORT || 8080;
+
+app.post('/get-file-size', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            if (err.code == 'LIMIT_FILE_SIZE') {
+                return res.send({
+                    'error': 'The maximum file size limit for uploads is ' + fileSizeLimit,
+                    'details': err
+                });
+            }
+            return res.send({ 'error': err });
+        }
+
+        res.json({ 'size': req.file.size });
+    })
+})
+app.listen(port);
+
